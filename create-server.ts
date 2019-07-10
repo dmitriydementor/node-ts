ee46c1c1d027d75bebaebaeefcdc47a50885f597
dmitriydementor <dmitriydementor@gmail.com>
2019-07-10 13:41:42 +0300

    Part 8. https server

---
+++ create-server.ts
@@ -0,0 +1,52 @@
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+
+import { getJsonDataFromStream } from './utils/getJsonDataFromStream';
+import { parseForm } from './utils/parseForm';
+
+const PORT = process.env.PORT || 5000;
+
+const posts = [{
+    title: 'Lorem ipsum',
+    content: 'Dolor sit amet',
+}];
+
+const server = createServer((req: IncomingMessage, res: ServerResponse) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    switch (req.url) {
+        case '/info': {
+            res.end(JSON.stringify(process.env));
+            break;
+        }
+        case '/posts': {
+            if (req.method === 'GET') {
+                res.end(JSON.stringify(posts));
+            } else if (req.method === 'POST') {
+                getJsonDataFromStream<{ title: string; content: string}>(req).then(post => {
+                    (post as any).createdAt = new Date();
+                    posts.push(post);
+                    res.end(JSON.stringify(post));
+                });
+            }
+            break;
+        }
+        case '/upload': {
+            if (req.method === 'POST') {
+                parseForm(req, res);
+            }
+            break;
+        }
+        default: {
+            res.statusCode = 404;
+            res.end(JSON.stringify({ error: 'Not Found' }));
+        }
+    }
+});
+
+server.listen(PORT, (error: any) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(`Server listening on port ${PORT}`);
+    }
+});
